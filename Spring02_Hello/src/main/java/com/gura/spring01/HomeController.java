f10011a8f5b9fbbf839cfa7b8303e98462209844
kimgura <oli999@naver.com>
2021-01-21 17:34:50 +0900

    Step02  프로젝트 추가함

---
+++ Spring02_Hello/src/main/java/com/gura/spring01/HomeController.java
@@ -0,0 +1,20 @@
+package com.gura.spring01;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	// home.do 요청이 오면 
+	@RequestMapping("/home.do") // .do 는 생략 가능
+	public String home() {
+		
+		// WEB-INF/views/home.jsp 페이지로 forward 이동해서 응답하기
+		return "home";
+	}
+}
+
+
+
+
+
