33af1b2d834e0272c5850f73d3f8694319a1adf7
kimgura <oli999@naver.com>
2021-01-22 10:51:05 +0900

    Spring02 /person.do 요청 처리함

---
+++ Spring02_Hello/src/main/java/com/gura/spring01/PersonController.java
@@ -0,0 +1,26 @@
+package com.gura.spring01;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/* component scan 될때 PersonController 객체가 생성이되고 관리가 된다. */
+@Controller
+public class PersonController {
+	
+	@RequestMapping("/person") 
+	public String person(HttpServletRequest request) {
+		//view page 에 전달할 data (Model)
+		String personToday="김구라";
+		//request scope 에 담는다.
+		request.setAttribute("personToday", personToday);
+		//forward 이동할 view page 의 위치를 리턴해준다.
+		return "person"; // /WEB-INF/view/person.jsp 
+	}
+}
+
+
+
+
+
---
+++ Spring02_Hello/src/main/webapp/WEB-INF/views/person.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>/views/person.jsp</title>
+</head>
+<body>
+<h1>오늘의 인물 페이지</h1>
+<p>오늘의 인물 : <strong>${personToday }</strong></p>
+<a href="home.do">인덱스로 가기</a>
+</body>
+</html>
