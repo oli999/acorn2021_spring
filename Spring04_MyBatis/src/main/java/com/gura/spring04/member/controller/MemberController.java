d48d625b241392753d7b9235bd0185c67b571a3d
kimgura <oli999@naver.com>
2021-01-25 11:56:52 +0900

    Spring04 /member/list.do 구현함

--- Spring04_MyBatis/src/main/java/com/gura/spring04/member/controller/MemberController.java
+++ Spring04_MyBatis/src/main/java/com/gura/spring04/member/controller/MemberController.java
@@ -1,11 +1,14 @@
 package com.gura.spring04.member.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.gura.spring04.member.dao.MemberDao;
+import com.gura.spring04.member.dto.MemberDto;
 
 @Controller
 public class MemberController {
@@ -14,9 +17,13 @@ public class MemberController {
 	private MemberDao dao;
 	
 	@RequestMapping("/member/list")
-	public ModelAndView list() {
-		
-		return null;
+	public ModelAndView list(ModelAndView mView) {
+		//1. 회원 목록을 얻어온다.
+		List<MemberDto> list=dao.getList();
+		//2. 회원 목록을 request scope 에 담고 view page 로 forward 이동해서 응답
+		mView.addObject("list", list);
+		mView.setViewName("member/list");
+		return mView;
 	}
 }
 
