a4d111bf32554994271538900c953754c6f94cca
kimgura <oli999@naver.com>
2021-01-26 16:25:18 +0900

    Spring05 UsersDaoImpl 완성함

--- Spring05_Final/src/main/java/com/gura/spring05/users/dao/UsersDaoImpl.java
+++ Spring05_Final/src/main/java/com/gura/spring05/users/dao/UsersDaoImpl.java
@@ -15,50 +15,100 @@ public class UsersDaoImpl implements UsersDao{
 	
 	@Override
 	public void updateProfile(UsersDto dto) {
-		// TODO Auto-generated method stub
-		
+		/*
+		 *  mapper namespace => users
+		 *  sql id => updateProfile
+		 *  parameterType => UsersDto
+		 */
+		session.update("users.updateProfile", dto);
 	}
 
 	@Override
 	public boolean isExist(String id) {
-		// TODO Auto-generated method stub
-		return false;
+		/*
+		 *  mapper namespace => users
+		 *  sql id => isExist
+		 *  parameterType => String
+		 *  resultType => UsersDto
+		 */
+		// id 가 존재하면(이미등록된아이디) null 이 아니고 존재하지 않으면 null  이다.
+		UsersDto dto=session.selectOne("users.isExist", id);
+		if(dto==null) {
+			return false;
+		}else {
+			return true;
+		}
 	}
 
 	@Override
 	public void updatePwd(UsersDto dto) {
-		// TODO Auto-generated method stub
-		
+		/*
+		 *  mapper namespace => users
+		 *  sql id => updatePwd
+		 *  parameterType => UsersDto
+		 */
+		session.update("users.updatePwd", dto);
 	}
 
 	@Override
 	public void update(UsersDto dto) {
-		// TODO Auto-generated method stub
-		
+		/*
+		 *  mapper namespace => users
+		 *  sql id => update
+		 *  parameterType => UsersDto
+		 */
+		session.update("users.update", dto);
 	}
 
 	@Override
 	public void delete(String id) {
-		// TODO Auto-generated method stub
-		
+		/*
+		 *  mapper namespace => users
+		 *  sql id => delete
+		 *  parameterType => String
+		 */
+		session.delete("users.delete", id);
 	}
 
 	@Override
 	public UsersDto getData(String id) {
-		// TODO Auto-generated method stub
-		return null;
+		/*
+		 *  mapper namespace => users
+		 *  sql id => getData
+		 *  parameterType => String
+		 *  resultType => UsersDto
+		 */
+		UsersDto dto=session.selectOne("users.getData", id);
+		return dto;
 	}
 
 	@Override
 	public boolean isValid(UsersDto dto) {
-		// TODO Auto-generated method stub
-		return false;
+		/*
+		 *  mapper namespace => users
+		 *  sql id => isValid
+		 *  parameterType => UsersDto
+		 *  resultType => String
+		 */
+		String id=session.selectOne("users.isValid", dto);
+		if(id==null) { //잘못된 아이디와 비밀번호
+			return false;
+		}else {//유효한 아이디와 비밀번호 
+			return true;
+		}
 	}
 
 	@Override
 	public void insert(UsersDto dto) {
-		// TODO Auto-generated method stub
-		
+		/*
+		 *  mapper namespace => users
+		 *  sql id => insert
+		 *  parameterType => UsersDto
+		 */
+		session.insert("users.insert", dto);
 	}
 
 }
+
+
+
