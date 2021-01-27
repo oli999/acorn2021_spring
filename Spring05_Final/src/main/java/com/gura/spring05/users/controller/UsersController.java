29af05a1c31e9ba99675eb45f3ae2a815dd86729
kimgura <oli999@naver.com>
2021-01-27 10:11:17 +0900

    Spring05 /users/checkid.do 요청 처리 함 (JSON 응답)

--- Spring05_Final/src/main/java/com/gura/spring05/users/controller/UsersController.java
+++ Spring05_Final/src/main/java/com/gura/spring05/users/controller/UsersController.java
@@ -3,6 +3,8 @@ package com.gura.spring05.users.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.gura.spring05.users.service.UsersService;
 
@@ -15,6 +17,23 @@ public class UsersController {
 	@RequestMapping("/users/signup_form")
 	public String signupForm() {
 		return "users/signup_form";
+	}
+	//ajax 요청 처리 
+	@RequestMapping("/users/checkid")
+	public ModelAndView checkid(@RequestParam String inputId,
+			ModelAndView mView) {
+		/*
+		 * (@RequestParam String inputId) 
+		 * 는
+		 * String inputId=request.getParameter("inputId") 
+		 * 와 같다.
+		 */
+		//서비스를 이용해서 해당 아이디가 존재하는지 여부를 알아낸다.
+		boolean isExist=service.isExistId(inputId);
+		//ModelAndView 객체에 해당 정보를 담고 view page 로 forward 이동해서 응답
+		mView.addObject("isExist", isExist);
+		mView.setViewName("users/checkid");
+		return mView;
 	}
 }
 
