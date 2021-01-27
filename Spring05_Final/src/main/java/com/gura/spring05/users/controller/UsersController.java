b7017e2850bfd0b5bb5559df3ccedfe9a7e0adba
kimgura <oli999@naver.com>
2021-01-27 11:18:14 +0900

    Spring05 /users/loginform.do 요청 처리함

--- Spring05_Final/src/main/java/com/gura/spring05/users/controller/UsersController.java
+++ Spring05_Final/src/main/java/com/gura/spring05/users/controller/UsersController.java
@@ -1,5 +1,8 @@
 package com.gura.spring05.users.controller;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -16,6 +19,18 @@ public class UsersController {
 	
 	@Autowired
 	private UsersService service;
+	
+	//로그인 폼 요청 처리
+	@RequestMapping("/users/loginform")
+	public ModelAndView loginform(HttpServletRequest request, 
+			ModelAndView mView) {
+		//로그인 폼에 관련된 로직을 서비스를 통해서 처리한다.
+		service.loginformLogic(request, mView);
+		//view page 정보도 담는다.
+		mView.setViewName("users/loginform");
+		//리턴
+		return mView;
+	}
 	
 	//회원 가입 요청처리
 	//form 전송은 보통 post 방식 요청인데 post 방식 요청만 받아들이도록 
