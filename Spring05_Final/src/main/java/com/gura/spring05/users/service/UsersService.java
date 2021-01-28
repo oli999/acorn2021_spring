9263e7238495992ab4ab1f9be22a7749001dd8d5
kimgura <oli999@naver.com>
2021-01-28 12:52:21 +0900

    Spring05 프로필 이미지 수정 기능 구현함

--- Spring05_Final/src/main/java/com/gura/spring05/users/service/UsersService.java
+++ Spring05_Final/src/main/java/com/gura/spring05/users/service/UsersService.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.gura.spring05.users.dto.UsersDto;
@@ -26,6 +27,9 @@ public interface UsersService {
 	//비밀번호를 수정하는 처리를 하고 성공 여부를 ModelAndView 객체에 담는 메소드
 	public void updateUserPwd(ModelAndView mView, UsersDto dto, 
 			HttpSession session);
+	//프로필 이미지를 upload 폴더에 저장하고 저장된 파일명을 DB 에 저장하는 메소드
+	public void saveProfileImage(MultipartFile image, 
+			HttpServletRequest request);
 }
 
 
