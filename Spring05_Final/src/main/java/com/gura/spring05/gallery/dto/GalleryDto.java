f19870718ff18592ca2f1d07625d9690adf4f910
kimgura <oli999@naver.com>
2021-02-03 18:19:29 +0900

    Spring05 사진 업로드 기능 구현중...

--- Spring05_Final/src/main/java/com/gura/spring05/gallery/dto/GalleryDto.java
+++ Spring05_Final/src/main/java/com/gura/spring05/gallery/dto/GalleryDto.java
@@ -1,5 +1,7 @@
 package com.gura.spring05.gallery.dto;
 
+import org.springframework.web.multipart.MultipartFile;
+
 public class GalleryDto {
 	private int num;
 	private String writer;
@@ -10,10 +12,11 @@ public class GalleryDto {
 	private int endRowNum;
 	private int prevNum; //이전글의 글번호
 	private int nextNum; //다음글의 글번호
+	private MultipartFile image;
 	//디폴트 생성자 
 	public GalleryDto() {}
 	public GalleryDto(int num, String writer, String caption, String imagePath, String regdate, int startRowNum,
-			int endRowNum, int prevNum, int nextNum) {
+			int endRowNum, int prevNum, int nextNum, MultipartFile image) {
 		super();
 		this.num = num;
 		this.writer = writer;
@@ -24,6 +27,7 @@ public class GalleryDto {
 		this.endRowNum = endRowNum;
 		this.prevNum = prevNum;
 		this.nextNum = nextNum;
+		this.image = image;
 	}
 	public int getNum() {
 		return num;
@@ -78,6 +82,12 @@ public class GalleryDto {
 	}
 	public void setNextNum(int nextNum) {
 		this.nextNum = nextNum;
+	}
+	public MultipartFile getImage() {
+		return image;
+	}
+	public void setImage(MultipartFile image) {
+		this.image = image;
 	}
 	
 }
