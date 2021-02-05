090270318ecce83721e73e803046127ccb758d0e
kimgura <oli999@naver.com>
2021-02-05 15:21:38 +0900

    Spring05 Aspect 예제 추가함

--- Spring05_Final/src/main/java/com/gura/spring05/exception/ExceptionController.java
+++ Spring05_Final/src/main/java/com/gura/spring05/exception/ExceptionController.java
@@ -9,6 +9,15 @@ import org.springframework.web.servlet.ModelAndView;
  */
 @ControllerAdvice
 public class ExceptionController {
+	//NowAllowException type 의 예외가 발생하면 호출되는 메소드 
+	@ExceptionHandler(NotAllowException.class)
+	public ModelAndView notAllow(NotAllowException ne) {
+		ModelAndView mView=new ModelAndView();
+		mView.addObject("exception", ne);
+		mView.setViewName("error/not_allow");
+		return mView;
+	}
+	
 	// DBFailException type 의 예외가 발생하면 호출되는 메소드 
 	@ExceptionHandler(DBFailException.class)
 	public ModelAndView dbFail(DBFailException e) {
